cca4c9668c46eb77753c5f41eb1af68a38b13791
ZhanpZ <66219129+ZhanpZ@users.noreply.github.com>
2021-09-17 13:52:20 -0400

    commited 9/17

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Zhanping Zeng on 9/15/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
--- flixster/MovieViewController.swift
+++ flixster/MovieViewController.swift
@@ -73,6 +73,21 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
         
         return cell
     }
+    
+    override func prepare(for segue:
+            UIStoryboardSegue, sender:Any?){
+        
+        print("Loading up the detail screen")
+        
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for:cell)!
+        let movie = movies[indexPath.row]
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
  
 
 }
